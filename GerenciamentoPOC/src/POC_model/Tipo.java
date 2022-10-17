ee5e13b122124309bc84d6248b9dd2d387e0567f
cleidimar-passos <cleidimar.passos@ufv.br>
2022-10-17 16:39:41 -0300

    classe aluno e professor substituidos por usuario e tipo

---
+++ GerenciamentoPOC/src/POC_model/Tipo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POC_model;
+
+/**
+ *
+ * @author cleid
+ */
+public class Tipo {
+    public enum tipo{
+        Professor, Aluno
+    }
+}
