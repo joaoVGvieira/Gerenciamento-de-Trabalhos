3354983af8b3a213f565aec03b73641242847242
Leonardo-Araujo-Resende <leonardo.a.aguiar@ufv.br>
2022-11-07 19:38:26 -0300

    MExendo na view

--- GerenciamentoPOC/src/main/java/Model/Aluno.java
+++ GerenciamentoPOC/src/main/java/Model/Aluno.java
@@ -10,4 +10,8 @@ public class Aluno extends Usuario{
     public Aluno(String nome, String matricula, String senha, TipoUsuario tipo){
         super(nome, matricula, senha, tipo);
     }
+    
+    public String toString(){
+        return "Nome " + getNome()+ "\n" + "Matricula " + getMatricula() + "\n" + "Tipo " + ;
+    }
 }
--- GerenciamentoPOC/src/main/java/View/InterfaceAluno.java
+++ GerenciamentoPOC/src/main/java/View/InterfaceAluno.java
@@ -1,7 +1,27 @@
-
 package View;
 
-//TODO interface que aparecera para o aluno
+import DAO.Context;
+import Model.Aluno;
+import Model.TipoUsuario;
+import java.util.Scanner;
+
 public class InterfaceAluno {
     
+    public void mostrarOpcaoAluno(){
+        System.out.println("----- Opcoes do Aluno -----");
+        System.out.println("1- Pesquisar POC");
+        System.out.println("2- Informações do aluno");
+        System.out.println("3- Trocar a senha");
+        System.out.println("---------------------------");
+    }
+    
+    public void mostrarOpcao1(){
+        System.out.println("");
+    }
+    
+    public void mostrarOpcao2(Aluno alunoLogado){
+        System.out.println("Nome: " + alunoLogado.getNome());
+        System.out.println("Matricula: ");
+        System.out.println("Senha: ");
+    }
 }
